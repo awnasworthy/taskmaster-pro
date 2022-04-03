857859ca227d6fede9603a53c0a7e1624000b3cf
awnasworthy <allyson.nasworthy@gmail.com>
2022-04-03 16:00:30 -0400

    complete

--- assets/js/script.js
+++ assets/js/script.js
@@ -103,7 +103,7 @@ $("#task-form-modal").on("shown.bs.modal", function() {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function() {
+$("#task-form-modal .btn-save").click(function() {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
@@ -204,10 +204,12 @@ $(".card .list-group").sortable({
   activate: function(event) {
     console.log("activate", this);
     $(this).addClass("dropover")
+    $('.bottom-trash').addClass("bottom-trash-drag")
   },
   deactivate: function(event) {
     console.log("deactivate", this);
     $(this).removeClass('dropover')
+    $('.bottom-trash').removeClass("bottom-trash-drag")
   },
   over: function(event) {
     console.log("over", event.target);
@@ -259,12 +261,15 @@ $("#trash").droppable({
   drop: function(event, ui) {
     console.log("drop");
     ui.draggable.remove();
+    $('.bottom-trash').removeClass("bottom-trash-active");
   },
   over: function(event, ui) {
     console.log("over");
+    $('.bottom-trash').addClass("bottom-trash-active");
   },
   out: function(event, ui) {
     console.log("out");
+    $('.bottom-trash').removeClass("bottom-trash-active");
   }
 });
 
